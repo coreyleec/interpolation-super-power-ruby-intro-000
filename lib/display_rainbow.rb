0b38dc3bfb9669a5bd6f0523df32b7aeafb86c14
Corey Colligan <coreyleec91288@gmail.com>
2021-03-04 22:38:31 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,10 +1,10 @@
 def display_rainbow
 puts "#{R}, #{O}, #{Y}, #{G}, #{B}, #{I}, #{V}"
 end
-R = "R: red"
-O = "O: orange"
-Y = "Y: yellow"
-G = "G: green"
-B = "B: blue"
-I = "I: indigo"
-V = "V: violet"
+R = "red"
+O = "orange"
+Y = "yellow"
+G = "green"
+B = "blue"
+I = "indigo"
+V = "violet"
